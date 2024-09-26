85d51c351dc4015ebc84cbd5c8031b8187d156f1
Pedro Coiado <phrc.mc@gmail.com>
2024-09-26 11:50:14 -0300

    .venv em execução

---
+++ funcoes.py
@@ -0,0 +1,8 @@
+def soma(n1, n2):
+    return n1 + n2
+
+def somaLista(Lista):
+    rs = 0 
+    for i in Lista:
+        rs+=i
+    print(f"O resultado da soma é {rs}")
---
+++ usar_funcoes.py
@@ -0,0 +1,5 @@
+# import funcoes as fn
+from funcoes import soma as sm, listaSoma as ls
+
+s = fn.soma(10,2)
+print(f"resultado é {s}")
